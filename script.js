7bd03e7de197565e32acb317496225ab05b784ed
manfredjoa <joamanfred@gmail.com>
2023-07-10 14:13:54 -0400

    add function to draw when incorrect letters are selected

--- script.js
+++ script.js
@@ -39,13 +39,14 @@ buttons.forEach(button => {
       if (e.target.id === word.charAt([i])) {
         paragraphs[i].innerHTML = '' // This removes the "__" before replacing with a letter.
         paragraphs[i].innerHTML = e.target.id.toUpperCase()
-      
+
       } else {
         wrongLetterCounter++
-      
+
       } if (wrongLetterCounter === word.length) {
         wrongLetters.push(e.target.id)
         wrongLetterCounter = 0
+        hangman()
       }
     }
     if (wrongLetterCounter > 0 && wrongLetterCounter < word.length) {
@@ -59,35 +60,36 @@ buttons.forEach(button => {
 const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 document.addEventListener("keydown", (e) => {
-  if (alphabet.includes(e.key) === false)  { return }
-    
+  if (alphabet.includes(e.key) === false) { return }
+
   for (let i = 0; i < word.length; i++) {
     if (e.key === word.charAt([i])) {
       paragraphs[i].innerHTML = '' // This removes the "__" before replacing with a letter.
       paragraphs[i].innerHTML = e.key.toUpperCase()
-      
+
     } else {
       wrongLetterCounter++
-    
+
     } if (wrongLetters.includes(e.key) === true) {
       wrongLetterCounter = 0
 
     } else if (wrongLetterCounter === word.length) {
       wrongLetters.push(e.key)
       wrongLetterCounter = 0
+      hangman()
     }
   } if (wrongLetterCounter > 0 && wrongLetterCounter < word.length) {
     wrongLetterCounter = 0 // This prevents the counter from being carried over if it did not equal word.length.
-  } 
-  
+  }
+
   buttons.forEach(button => {
     if (e.key === button.id) {
-      button.setAttribute("disabled", "disabled")
+      button.setAttribute("disabled", "disabled") // This disables buttons on keydown.
     }
   })
   console.log(wrongLetters)
 })
-    // I feel like I may be able to combine these into 1 function? I can have conditionals using e.type, but keydown has to be added onto the document.
+// I feel like I may be able to combine these into 1 function? I can have conditionals using e.type, but keydown has to be added onto the document.
 
 newGame.addEventListener("click", () => {
   for (let i = word.length - 1; i >= 0; i--) {
@@ -97,18 +99,59 @@ newGame.addEventListener("click", () => {
 })
 
 
-// function draw() {
-  //   const canvas = document.getElementById("hangman")
-  //   if (canvas.getContext("2d")) {
-    //     const hangman = canvas.getContext("2d")
-    //   }
-    // }
-    // draw()
+function hangman() {
+  const canvas = document.getElementById("hangman")
+  if (canvas.getContext("2d")) {
+    const draw = canvas.getContext("2d")
+    if (wrongLetters.length === 0) {
+      draw.beginPath()
+      draw.moveTo(5, 140)
+      draw.lineTo(295, 140)
+      draw.moveTo(25, 140)
+      draw.lineTo(25, 10)
+      draw.lineTo(150, 10)
+      draw.lineTo(150, 25)
+      draw.stroke()
 
-// Learning how to draw on a canvas!
-    
+    } if (wrongLetters.length === 1) {
+      draw.beginPath()
+      draw.arc(150, 40, 15, 0, Math.PI * 2, true)
+      draw.stroke()
+
+    } if (wrongLetters.length === 2) {
+      draw.beginPath()
+      draw.moveTo(150, 55)
+      draw.lineTo(150, 95)
+      draw.stroke()
+
+    } if (wrongLetters.length === 3) {
+      draw.beginPath()
+      draw.moveTo(150, 70)
+      draw.lineTo(135, 60)
+      draw.stroke()
+
+    } if (wrongLetters.length === 4) {
+      draw.beginPath()
+      draw.moveTo(150, 70)
+      draw.lineTo(165, 60)
+      draw.stroke()
+
+    } if (wrongLetters.length === 5) {
+      draw.beginPath()
+      draw.moveTo(150, 95)
+      draw.lineTo(130, 125)
+      draw.stroke()
+
+    } if (wrongLetters.length === 6) {
+      draw.beginPath()
+      draw.moveTo(150, 95)
+      draw.lineTo(170, 125)
+      draw.stroke()
+    }
+  }
+}
+
+hangman() // This will give us the initial drawing when the page is loaded.
+
 // Need to add a function that prevents buttons from being clicked/keydowned after game is solved(POINTER EVENTS: NONE).
 // How does the code know when the game is solved? Some sort of checking function, but I'm not sure what it would check for.
-    
-// I think I can make 1 draw function with if conditionals and have an empty array where incorrect letters are pushed into it.
-// Depending on the length of the array, certain strokes will be drawn.
