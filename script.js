77a51deef705777712972fc485170649b051f5cc
manfredjoa <joamanfred@gmail.com>
2023-07-10 22:38:46 -0400

    add win and lose functions

--- script.js
+++ script.js
@@ -5,12 +5,18 @@ const buttons = document.querySelectorAll("button")
 const newGame = document.querySelector("#newGame")
 const paragraphs = document.getElementsByTagName("p")
 const blankSpaces = document.querySelector(".blankSpaces")
-const hidden = document.querySelector(".hidden")
+const hiddenSection = document.querySelector(".hiddenSection")
+const winGame = document.querySelector("#win")
+const loseGame = document.querySelector("#lose")
+const playAgain = document.querySelector("#playAgain")
+
 let word = ''
 let wrongLetters = [] // Draw function will be executed based off of the length of this.
 let wrongLetterCounter = 0
+let winCount = 0
 
 
+// Button click and keydown functions
 buttons.forEach(button => {
   button.addEventListener("click", (e) => {
     if (e.target.id === "newGame") { return } // This prevents the New Game button from being disabled.
@@ -20,7 +26,6 @@ buttons.forEach(button => {
         paragraphs[i].innerHTML = '' // This removes the "__" before replacing with a letter.
         paragraphs[i].innerHTML = e.target.id.toUpperCase()
         paragraphs[i].classList.add("changeFont")
-        button.classList.add("changeFontColor")
 
       } else {
         wrongLetterCounter++
@@ -34,6 +39,7 @@ buttons.forEach(button => {
     if (wrongLetterCounter > 0 && wrongLetterCounter < word.length) {
       wrongLetterCounter = 0 // This prevents the counter from being carried over if it did not equal word.length.
     }
+    win()
     button.classList.add("changeFontColor")
     button.setAttribute("disabled", "disabled") // This disables buttons on click.
   })
@@ -63,6 +69,7 @@ document.addEventListener("keydown", handleKeyDown = (e) => {
   } if (wrongLetterCounter > 0 && wrongLetterCounter < word.length) {
     wrongLetterCounter = 0 // This prevents the counter from being carried over if it did not equal word.length.
   }
+  win()
 
   buttons.forEach(button => {
     if (e.key === button.id) {
@@ -70,10 +77,10 @@ document.addEventListener("keydown", handleKeyDown = (e) => {
       button.setAttribute("disabled", "disabled") // This disables buttons on keydown.
     }
   })
-  console.log(wrongLetters)
 })
-// I feel like I may be able to combine these into 1 function? I can have conditionals using e.type, but keydown has to be added onto the document.
 
+
+// New game function
 function createNewGame() {
   word = guessingWords[Math.floor(Math.random() * guessingWords.length)]
   console.log(word)
@@ -81,9 +88,6 @@ function createNewGame() {
   for (let i = 0; i < word.length; i++) {
     const newParagraph = document.createElement("p")
     blankSpaces.appendChild(newParagraph)
-
-  } for (let i = 0; i < word.length; i++) {
-    paragraphs[i].innerText = ''
 
   } for (let i = 0; i < word.length; i++) {
     paragraphs[i].innerText = "__"
@@ -94,21 +98,31 @@ function createNewGame() {
     wrongLetters = []
   })
   document.addEventListener("keydown", handleKeyDown)
-  hidden.classList.remove("overlay")
   hangman()
 }
 // Need to find a way where it won't select a word that was just chosen.
 
 createNewGame() // This creates the first game when you open the browser.
 
-newGame.addEventListener("click", () => {
+
+// New game button functions
+const newGameButtonClick = () => {
   for (let i = word.length - 1; i >= 0; i--) {
     paragraphs[i].remove() // This removes all the p tags before new ones are created.
   }
+  hiddenSection.classList.add("hidden")
+  hiddenSection.classList.remove("overlay")
+  winGame.classList.add("hidden")
+  loseGame.classList.add("hidden")
+  playAgain.classList.add("hidden")
   createNewGame()
-})
+}
+
+newGame.addEventListener("click", newGameButtonClick)
+playAgain.addEventListener("click", newGameButtonClick)
 
 
+// Drawing hangman function
 function hangman() {
   const canvas = document.getElementById("hangman")
   if (canvas.getContext("2d")) {
@@ -116,16 +130,15 @@ function hangman() {
     if (wrongLetters.length === 0) {
       draw.clearRect(0, 0, canvas.width, canvas.height)
 
-      draw.lineWidth = 10; draw.strokeStyle = "rgb(52, 29, 29)"
-      draw.beginPath();
-      draw.moveTo(0, 325); draw.lineTo(934, 325);
-      draw.moveTo(10, 315); draw.lineTo(924, 315);
-      draw.moveTo(20, 305); draw.lineTo(914, 305);
-      draw.moveTo(30, 295); draw.lineTo(904, 295); draw.stroke();
+      draw.lineWidth = 10; draw.strokeStyle = "rgb(52, 29, 29)"; draw.beginPath();
+      draw.moveTo(0, 315); draw.lineTo(934, 315);
+      draw.moveTo(10, 305); draw.lineTo(924, 305);
+      draw.moveTo(20, 295); draw.lineTo(914, 295);
+      draw.moveTo(30, 285); draw.lineTo(904, 285); draw.stroke();
 
-      draw.lineWidth = 25
-      draw.moveTo(300, 305); draw.lineTo(300, 50);
-      draw.moveTo(270, 290); draw.lineTo(330, 290); draw.stroke();
+      draw.lineWidth = 25; draw.strokeStyle = "rgb(0, 0, 0)"; draw.beginPath()
+      draw.moveTo(300, 280); draw.lineTo(300, 50);
+      draw.moveTo(270, 280); draw.lineTo(330, 280); draw.stroke();
 
       draw.lineWidth = 15
       draw.moveTo(287.5, 50); draw.lineTo(467, 50); draw.stroke();
@@ -156,13 +169,38 @@ function hangman() {
     } if (wrongLetters.length === 6) {
       draw.beginPath(); draw.moveTo(461, 190); draw.lineTo(477, 250); draw.stroke()
 
-      hidden.classList.remove("hidden")
-      hidden.classList.add("overlay")
-      document.removeEventListener("keydown", handleKeyDown)
+      lose()
     }
   }
 }
 
 hangman() // This will give us the initial drawing when the page is loaded.
 
-// How does the code know when the game is solved? Some sort of checking function, but I'm not sure what it would check for. INNERTEXT?
+
+// Win function
+function win() {
+  for (let i = 0; i < word.length; i++) {
+    if (paragraphs[i].innerText != "__") {
+      winCount++
+    }
+  } if (winCount !== word.length) {
+    winCount = 0
+
+  } else if (winCount === word.length) {
+    hiddenSection.classList.remove("hidden")
+    hiddenSection.classList.add("overlay")
+    winGame.classList.remove("hidden")
+    playAgain.classList.remove("hidden")
+    winCount = 0
+  }
+}
+
+
+// Lose function
+function lose() {
+  hiddenSection.classList.remove("hidden")
+  hiddenSection.classList.add("overlay")
+  loseGame.classList.remove("hidden")
+  playAgain.classList.remove("hidden")
+  document.removeEventListener("keydown", handleKeyDown)
+}
